b5c0fc6314c9175d13135d84e3adb506e18b69b0
kyok01 <kazutakayokoils@gmail.com>
2022-09-26 00:41:41 +0900

    deploy

--- frontend/pages/posts/[id].tsx
+++ frontend/pages/posts/[id].tsx
@@ -5,15 +5,24 @@ import Artifact from "@cont/ArchiveCoin.json";
 import contractAddress from "@cont/contract-address.json";
 import { useState, useEffect } from "react";
 import { postType } from "types/postType";
+import { Navbar } from "components/organisms/NavBar";
+import { filter } from "util/filterArr";
+import { WritePostForm } from "components/organisms/WritePostForm";
+import { sendPost } from "util/sendPost";
+import { WriteCommentForm } from "components/organisms/WriteCommentForm";
 
 export default ({ pId }) => {
   const [post, setPost] = useState<postType>({});
+  const [repPs, setRepPs] = useState([]);
+  const [reTitle, setReT] = useState<string>("");
+  const [isRepP, setIsRepP] = useState(true);
+  const [isRepC, setIsRepC] = useState(false);
   useEffect(() => {
-    getQuesitonForId();
+    getPostForPId();
     // getAnswerForId();
   }, []);
 
-  async function getQuesitonForId() {
+  async function getPostForPId() {
     //After adding your Hardhat network to your metamask, this code will get providers and signers
     const provider = new ethers.providers.Web3Provider(window.ethereum);
     const signer = provider.getSigner();
@@ -35,7 +44,64 @@ export default ({ pId }) => {
     };
 
     console.log(post);
+    getReplyToInfo(post.replyTo);
     setPost(post);
+  }
+
+  async function getReplyToInfo(replyTo) {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+    //Pull the deployed contract instance
+    let contract = new ethers.Contract(
+      contractAddress.address,
+      Artifact.abi,
+      signer
+    );
+    //create an NFT Token
+    let transaction = await contract.getPostForPId(replyTo);
+    setReT(transaction.title);
+  }
+
+  async function getRepPosts() {
+    //After adding your Hardhat network to your metamask, this code will get providers and signers
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+    //Pull the deployed contract instance
+    let contract = new ethers.Contract(
+      contractAddress.address,
+      Artifact.abi,
+      signer
+    );
+    //create an NFT Token
+    const addr = await signer.getAddress();
+    let transaction = await contract.getAllPosts();
+
+    const posts = await Promise.all(
+      transaction.map(async (p, i) => {
+        const dateTime = new Date(p.timestamp * 1000);
+        let item = {
+          pId: i + 1,
+          title: p.title,
+          text: p.text,
+          sender: p.sender,
+          replyTo: p.replyTo.toNumber(),
+          timestamp: dateTime.toLocaleDateString(),
+        };
+        return item;
+      })
+    );
+
+    const repPosts = await filter(posts, async (p, i) => {
+      const bool = p.replyTo == pId;
+      console.log(bool);
+      return bool;
+    });
+
+    setRepPs(repPosts);
+  }
+
+  async function getReplies() {
+    await getRepPosts();
   }
 
   //   async function getAnswerForId() {
@@ -117,14 +183,59 @@ export default ({ pId }) => {
   //     }
   //   }
 
+  function clickRepP() {
+    setIsRepP(true);
+    setIsRepC(false);
+  }
+
+  function clickRepC() {
+    setIsRepP(false);
+    setIsRepC(true);
+  }
+
   return (
-    <div>
-      //{" "}
-      <div>
-        // <p>{post.title}</p>
-        //{" "}
+    <>
+      <Navbar />
+      <div className="mx-2">
+      <div className="w-full max-w-4xl mx-auto">
+        <div>{reTitle}</div>
+        <h1 className="text-2xl font-semibold">{post.title}</h1>
+        <div>
+          <div>{post.sender}</div>
+          <div>{post.timestamp}</div>
+        </div>
+        <p className="my-2">{post.text}</p>
       </div>
-    </div>
+      </div>
+
+      <div className="flex justify-center my-2">
+        <button
+          className="btn btn-secondary max-w-xs w-full"
+          onClick={getReplies}
+        >
+          Show Replies
+        </button>
+      </div>
+      <div className="flex justify-center my-2">
+        {repPs.map((repP, i) => (
+          <div>{repP.title}</div>
+        ))}
+      </div>
+      <div className="flex items-center my-2 flex-col">
+        <div className="btn-group">
+          <button className="btn btn-active w-40" onClick={() => clickRepP()}>
+            Reply as Post
+          </button>
+          <button className="btn w-40" onClick={() => clickRepC()}>
+            Comment
+          </button>
+        </div>
+      </div>
+      <div className="mx-2">
+        {isRepP && <WritePostForm onSubmit={(e) => sendPost(pId, e)} />}
+        {isRepC && <WriteCommentForm onSubmit={(e) => sendPost(pId, e)} />}
+      </div>
+    </>
   );
 };
 
@@ -133,13 +244,3 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
 
   return { props: { pId: id } };
 };
-
-// helper function
-async function filter(arr, callback) {
-  const fail = Symbol();
-  return (
-    await Promise.all(
-      arr.map(async (item) => ((await callback(item)) ? item : fail))
-    )
-  ).filter((i) => i !== fail);
-}
---
+++ frontend/util/sendComment.ts
@@ -0,0 +1,32 @@
+import { ethers } from "ethers";
+
+import Artifact from "@cont/ArchiveCoin.json";
+import contractAddress from "@cont/contract-address.json";
+
+export const sendPost = async(pId, event) => {
+  // Stop the form from submitting and refreshing the page.
+  event.preventDefault();
+
+  try {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+
+      let contract = new ethers.Contract(
+          contractAddress.address,
+          Artifact.abi,
+          signer
+        );
+        
+      const addr = await signer.getAddress();
+
+      const text = event.target.text.value;
+     
+      let transaction = await contract.setComment(text, pId);
+      await transaction.wait()
+
+      alert("Successfully send your Post!");
+  }
+  catch(e) {
+      alert( "Upload error"+e )
+  }
+}
