86e54c0a2281efb4eecfeb2983c742d1eff91af9
kyok01 <kazutakayokoils@gmail.com>
2022-09-26 17:04:18 +0900

    temporary save

--- frontend/pages/posts/[id].tsx
+++ frontend/pages/posts/[id].tsx
@@ -9,11 +9,13 @@ import { Navbar } from "components/organisms/NavBar";
 import { filter } from "util/filterArr";
 import { WritePostForm } from "components/organisms/WritePostForm";
 import { sendPost } from "util/sendPost";
+import { sendComment } from "util/sendComment";
 import { WriteCommentForm } from "components/organisms/WriteCommentForm";
 
 export default ({ pId }) => {
   const [post, setPost] = useState<postType>({});
   const [repPs, setRepPs] = useState([]);
+  const [repCs, setRepCs] = useState([]);
   const [reTitle, setReT] = useState<string>("");
   const [isRepP, setIsRepP] = useState(true);
   const [isRepC, setIsRepC] = useState(false);
@@ -73,7 +75,6 @@ export default ({ pId }) => {
       signer
     );
     //create an NFT Token
-    const addr = await signer.getAddress();
     let transaction = await contract.getAllPosts();
 
     const posts = await Promise.all(
@@ -100,88 +101,52 @@ export default ({ pId }) => {
     setRepPs(repPosts);
   }
 
-  async function getReplies() {
-    await getRepPosts();
+  async function getRepComments() {
+    //After adding your Hardhat network to your metamask, this code will get providers and signers
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+    //Pull the deployed contract instance
+    let contract = new ethers.Contract(
+      contractAddress.address,
+      Artifact.abi,
+      signer
+    );
+    //create an NFT Token
+    let transaction = await contract.getAllComments();
+
+    console.log(transaction);
+    
+
+    const comments = await Promise.all(
+      transaction.map(async (c, i) => {
+        const dateTime = new Date(c.timestamp * 1000);
+        let item = {
+          cId: i + 1,
+          text: c.text,
+          sender: c.sender,
+          replyTo: c.replyTo.toNumber(),
+          timestamp: dateTime.toLocaleDateString(),
+        };
+        return item;
+      })
+    );
+
+    console.log(comments);
+    
+
+    const repComments = await filter(comments, async (c, i) => {
+      const bool = c.replyTo == pId;
+      console.log(bool);
+      return bool;
+    });
+
+    setRepCs(repComments);
   }
 
-  //   async function getAnswerForId() {
-  //     //After adding your Hardhat network to your metamask, this code will get providers and signers
-  //     const provider = new ethers.providers.Web3Provider(window.ethereum);
-  //     const signer = provider.getSigner();
-  //     //Pull the deployed contract instance
-  //     let contract = new ethers.Contract(
-  //       contractAddress.address,
-  //       Artifact.abi,
-  //       signer
-  //     );
-  //     //create an NFT Token
-  //     let transaction = await contract.getAllAnswers();
-  //     let answerIds = [];
-
-  //     const rowAnswers = await filter(transaction, async (q, i) => {
-  //       answerIds.push(i);
-  //       const bool = (await q.tokenId.toNumber()) == pId;
-  //       console.log(bool);
-  //       return bool;
-  //     });
-
-  //     const answers = await Promise.all(
-  //       rowAnswers.map(async (q, i) => {
-  //         let item = {
-  //           answerId: answerIds[i],
-  //           text: q.text,
-  //           sender: q.sender,
-  //         };
-  //         return item;
-  //       })
-  //     );
-  //     console.log(answers);
-  //     setA(answers);
-  //   }
-
-  //   async function handleSubmit(event) {
-  //     // Stop the form from submitting and refreshing the page.
-  //     event.preventDefault();
-
-  //     try {
-  //       const provider = new ethers.providers.Web3Provider(window.ethereum);
-  //       const signer = provider.getSigner();
-
-  //       let contract = new ethers.Contract(
-  //         contractAddress.address,
-  //         Artifact.abi,
-  //         signer
-  //       );
-
-  //       const answerText = event.target.answerText.value;
-  //       const qacType = event.target.qacType.value;
-  //       const qacId = event.target.qacId.value;
-
-  //       let struct;
-  //       if (qacType === "question") {
-  //         struct = await contract.getQuestionForId(qacId);
-  //       } else if (qacType === "answer") {
-  //         struct = await contract.getAnswerForAnswerId(qacId);
-  //       } else if (qacType === "comment") {
-  //         struct = await contract.getCommentForCommentId(qacId);
-  //       }
-  //       const refAddr = struct.sender;
-
-  //       let transaction = await contract.setAnswer(
-  //         pId,
-  //         answerText,
-  //         qacType,
-  //         qacId,
-  //         refAddr
-  //       );
-  //       await transaction.wait();
-
-  //       alert("Successfully listed your Answer!");
-  //       location.reload();
-  //     } catch (e) {
-  //       alert("Upload error" + e);
-  //     }
-  //   }
+  async function getReplies() {
+    await getRepPosts();
+    await getRepComments();
+  }
 
   function clickRepP() {
     setIsRepP(true);
@@ -216,9 +181,12 @@ export default ({ pId }) => {
           Show Replies
         </button>
       </div>
-      <div className="flex justify-center my-2">
+      <div className="flex justify-center flex-col items-center my-2">
         {repPs.map((repP, i) => (
-          <div>{repP.title}</div>
+          <div key={i}>{repP.title}</div>
+        ))}
+        {repCs.map((repC, i) => (
+          <div key={i}>{repC.text}</div>
         ))}
       </div>
       <div className="flex items-center my-2 flex-col">
@@ -233,7 +201,7 @@ export default ({ pId }) => {
       </div>
       <div className="mx-2">
         {isRepP && <WritePostForm onSubmit={(e) => sendPost(pId, e)} />}
-        {isRepC && <WriteCommentForm onSubmit={(e) => sendPost(pId, e)} />}
+        {isRepC && <WriteCommentForm onSubmit={(e) => sendComment(pId, e)} />}
       </div>
     </>
   );
